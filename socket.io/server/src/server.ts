280507e2ce01ca4d98a1f05c6cea4e2f1e1b8d00
Adelino Neto <adelino.neto@showtecnologia.com>
2023-04-11 11:10:23 -0300

    Adicionado socket.io/server

---
+++ socket.io/server/src/server.ts
@@ -0,0 +1,23 @@
+import { Server } from 'socket.io';
+
+const PORT = 3000;
+
+const io = new Server(PORT);
+
+io.on('connection', (client) => {
+  console.log(`Client ${client.id} connected`);
+
+  client.on('disconnect', () => {
+    console.log(`[disconnect] Client ${client.id} disconnected`);
+  });
+
+  client.on('join', (room) => {
+    console.log(`[join] Client ${client.id} joined room ${room}`);
+    client.join(room);
+  });
+});
+
+// Each 30 seconds, emit a message to the room 2121
+setInterval(() => {
+  io.to('2121').emit('message', 'Hello world (2121)');
+}, 30000);
