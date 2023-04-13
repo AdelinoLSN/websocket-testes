5ccc63e1dfc3f565debed2a00dcf76e4c5908759
Adelino Neto <adelino.neto@showtecnologia.com>
2023-04-13 16:05:22 -0300

    wip nao funcionando

--- web/parte1.js
+++
@@ -1,7 +0,0 @@
-console.log('Loaded parte1.js');
-
-
-
-// sharedWorker.port.onmessage = (e) => {
-//     console.log(e.data);
-// }
--- web/sharedWorker.js
+++ web/sharedWorker.js
@@ -1,93 +1,62 @@
-var socket = io('ws://localhost:3000');
-var ports = [];
+console.log('Loaded parte2.js');
 
-addEventListener('connect', (event) => {
-    console.log('sharedWorker: addEventListener');
+// var browserInstances = [];
+    
+// onconnect = function(e) {
+//     const port = e.ports[0];
 
-    var port = event.ports[0];
-    ports.push(port);
-    port.start();
+//     if (!io) connectIo();
 
-    port.addEventListener('message', () => {
-        socket.on(event.data.events[0], (e) => {
-            port.postMessage(e);
-        });
-    });
-});
+//     browserInstances.push(port);
+
+//     postMessage(browserInstances, e.data);
+// }
+
+// function postMessage(browserInstances, data) {
+//     browserInstances.forEach((browserInstance) => {
+//         browserInstance.postMessage(data);
+//     });
+// }
+
+// function connectIo() {
+//     io = io.connect('http://localhost:3000');
+
+//     io.on('connect', function() {
+//         postMessage(browserInstances, 'connected');
+//     });
+// }
+
+importScripts('https://cdn.socket.io/4.6.0/socket.io.min.js');
+
+var socket = io.connect('http://localhost:3000');
+var browserInstances = [];
 
 socket.on('connect', () => {
-    console.log('sharedWorker: connect');
+    postMessage('connected');
 
-    ports.forEach((port) => {
-        port.postMessage({ type: 'connect', data: socket.id });
-    });
+    socket.emit('join', '2121');
 });
 
 socket.on('disconnect', () => {
-    console.log('sharedWorker: disconnect');
-    
-    ports.forEach((port) => {
-        port.postMessage({ type: 'disconnect' });
-    });
+    postMessage('disconnected');
 });
 
+socket.on('message', function(data) {
+    postMessage(data);
+});
 
+onconnect = function(e) {
+    const port = e.ports[0];
 
-// "use strict";
+    browserInstances.push(port);
 
-// importScripts('http://cdn.socket.io/socket.io-1.1.0.js');
+    port.onmessage = function(e) {
+        socket.emit('message', e.data);
+    }
+};
 
-// var socket = io(self.name),
-//     ports = [];
-
-// addEventListener('connect', function (event) {
-//     var port = event.ports[0];
-//     ports.push(port);
-//     port.start();
-
-//     port.addEventListener("message", function (event) {
-//         for (var i = 0; i < event.data.events.length; ++i) {
-//             var eventName = event.data.events[i];
-
-//             socket.on(event.data.events[i], function (e) {
-//                 port.postMessage({type: eventName, message: e});
-//             });
-//         }
-//     });
-// });
-
-// socket.on('connect', function () {
-//     for (var i = 0; i < ports.length; i++) {
-//         ports[i].postMessage({type: '_connect'});
-//     }
-// });
-
-// socket.on('disconnect', function () {
-//     for (var i = 0; i < ports.length; i++) {
-//         ports[i].postMessage({type: '_disconnect'});
-//     }
-// });
-
-
-
-// var socket;
-
-//     function connect() {
-//         socket = io.connect('http://localhost:3000');
-
-//         socket.on('connect', function() {
-//             console.log('Connected');
-
-//             socket.emit('join', '2121');
-//         });
-
-//         socket.on('disconnect', function() {
-//             console.log('Disconnected');
-//         });
-
-//         socket.on('message', function(data) {
-//             console.log(data);
-//         });
-//     }
-
-//     setTimeout(() => connect(), 5000);
+function postMessage(data) {
+    browserInstances.map((browserInstance) => {
+        browserInstance.postMessage(data);
+    });
+}
